9909931d452855f32651ba6eae67f6dc5db5803f
Zekromaster <dev@zekromaster.net>
2023-11-19 17:20:50 +0100

    feat: Memoization

--- src/main/java/net/zekromaster/minecraft/sheepeatgrass/api/SheepEatingRegistryImpl.java
+++ src/main/java/net/zekromaster/minecraft/sheepeatgrass/api/SheepEatingRegistryImpl.java
@@ -3,6 +3,7 @@ package net.zekromaster.minecraft.sheepeatgrass.api;
 import net.zekromaster.minecraft.sheepeatgrass.api.blocks.BlockReference;
 import net.zekromaster.minecraft.sheepeatgrass.api.blocks.EatingLocation;
 import net.zekromaster.minecraft.sheepeatgrass.api.blocks.matchers.BlockMatcher;
+import net.zekromaster.minecraft.sheepeatgrass.api.blocks.matchers.BlockReferenceMatcher;
 
 import java.util.*;
 
@@ -10,20 +11,34 @@ final class SheepEatingRegistryImpl implements SheepEatingRegistry {
 
     private final Set<RegistryEntry> registry = new HashSet<>();
 
+    private final Map<CacheKey, BlockReference> cache = new HashMap<>();
+
 
     @Override
     public void add(BlockMatcher matcher, EatingLocation location, BlockReference output) {
         registry.add(new RegistryEntry(matcher, location, output));
+
+        // Specifically in the case of BlockReferenceMatchers, we can cache the result
+        // right away
+        if (matcher instanceof BlockReferenceMatcher brm) {
+            cache.put(new CacheKey(brm.block(), location), output);
+        }
     }
 
     @Override
     public Optional<BlockReference> get(EatingLocation location, BlockReference block) {
-        return registry
-            .stream()
-            .filter(entry -> entry.location().equals(location))
-            .filter(entry -> entry.matcher().matches(block))
-            .map(RegistryEntry::output)
-            .findFirst();
+        return Optional.ofNullable(
+            this.cache.computeIfAbsent(
+                new CacheKey(block, location),
+                key -> registry
+                    .stream()
+                    .filter(entry -> entry.location().equals(key.location()))
+                    .filter(entry -> entry.matcher().matches(key.block()))
+                    .map(RegistryEntry::output)
+                    .findFirst()
+                    .orElse(null)
+                )
+        );
     }
 
     @Override
@@ -36,4 +51,10 @@ final class SheepEatingRegistryImpl implements SheepEatingRegistry {
         EatingLocation location,
         BlockReference output
     ) { }
+
+    private record CacheKey(
+        BlockReference block,
+        EatingLocation location
+    ) { }
+
 }
